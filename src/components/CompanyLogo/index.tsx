01a21ce377cab93c8c98787a4e5d4d88b9c768bc
Rafael Pelle <rafapelle@gmail.com>
2023-09-30 16:34:45 -0300

    feat: CompanyLogo img optimization

--- src/components/CompanyLogo/index.tsx
+++ src/components/CompanyLogo/index.tsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import companyLogoSrc from '../../assets/Logo_ML.png';
+import companyLogo2xSrc from '../../assets/Logo_ML@2x.png';
 
 function CompanyLogo() {
+  const [src, setSrc] = useState(companyLogoSrc);
+
   return (
     <div className="company-logo-container">
       <img
+        src={src}
         className="company-logo-img"
-        src={companyLogoSrc}
-        alt="Mercado Livre"
+        alt="Mercado Libre"
+        onLoad={() => {
+          if (companyLogo2xSrc.length > src.length) {
+            setSrc(companyLogo2xSrc);
+          }
+        }}
       />
     </div>
   );
